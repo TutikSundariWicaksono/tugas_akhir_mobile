6ddeaf4e9bb78c7e7ae01a425c6c573855f9b5c6
aida <yuaida.12@gmail.com>
2022-06-13 16:31:46 +0700

    question

--- lib/screens/quiz/components/body.dart
+++ lib/screens/quiz/components/body.dart
@@ -1,10 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:tugas_akhir_mobile/constants.dart';
+import 'package:tugas_akhir_mobile/controllers/question_controller.dart';
+import 'package:tugas_akhir_mobile/models/questions.dart';
+import 'package:tugas_akhir_mobile/screens/quiz/components/progress_bar.dart';
+import 'package:tugas_akhir_mobile/screens/quiz/components/question_card.dart';
 
 class Body extends StatelessWidget {
-  const Body({Key? key}) : super(key: key);
-
+  Color mainColor = Color(0xFF252c4a);
   @override
   Widget build(BuildContext context) {
-    return Container();
+    QuestionController _questionController = Get.put(QuestionController());
+
+    return Scaffold(
+      backgroundColor: mainColor,
+      body: Stack(
+        children: [
+          SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  child: ProgressBar(),
+                ),
+                SizedBox(
+                  height: kDefaultPadding,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  child: Obx(
+                    () => Text.rich(
+                      TextSpan(
+                        text:
+                            "Question ${_questionController.questionNumber.value}",
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline4
+                            ?.copyWith(color: kSecondaryColor),
+                        children: [
+                          TextSpan(
+                            text: "/${_questionController.questions.length}",
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline5
+                                ?.copyWith(color: kSecondaryColor),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Divider(thickness: 1.5),
+                SizedBox(height: kDefaultPadding),
+                Expanded(
+                  child: PageView.builder(
+                    // Block swipe to next qn
+                    physics: NeverScrollableScrollPhysics(),
+                    controller: _questionController.pageController,
+                    onPageChanged: _questionController.updateTheQnNum,
+                    itemCount: _questionController.questions.length,
+                    itemBuilder: (context, index) => QuestionCard(
+                        question: _questionController.questions[index]),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/screens/quiz/components/progress_bar.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:tugas_akhir_mobile/constants.dart';
+import 'package:tugas_akhir_mobile/controllers/question_controller.dart';
+
+class ProgressBar extends StatelessWidget {
+  const ProgressBar({
+    key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 35,
+      decoration: BoxDecoration(
+        border: Border.all(color: Color(0xFF3F4768), width: 3),
+        borderRadius: BorderRadius.circular(50),
+      ),
+      child: GetBuilder<QuestionController>(
+        init: QuestionController(),
+        builder: (controller) {
+          return Stack(
+            children: [
+              LayoutBuilder(
+                builder: (context, constraints) => Container(
+                  width: constraints.maxWidth * controller.animation.value,
+                  decoration: BoxDecoration(
+                    gradient: kPrimaryGradient,
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+              ),
+              Positioned.fill(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: kDefaultPadding / 2),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("${(controller.animation.value * 60).round()} sec"),
+                      SvgPicture.asset("assets/icons/clock.svg"),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
